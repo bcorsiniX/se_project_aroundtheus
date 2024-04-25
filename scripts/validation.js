a23b57c6f1c8f6c561c87bfb449c6abae7b0a3c3
Briana Orsini <bcorsinix@icloud.com>
2024-04-25 18:05:53 -0400

    fix validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,10 +1,46 @@
+const options = {
+    formSelector: ".modal",
+    inputSelector: ".modal__input",
+    submitButtonSelector: ".modal__save-button",
+    inactiveButtonClass: "modal__save-button_disabled",
+    inputErrorClass: "modal__input_type_error",
+    errorClass: "modal__error_visible"
+  };
+
+
+function showInputError(form, inputElement, {inputErrorClass, errorClass}) {
+
+    const errorElement = form.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add(inputErrorClass);
+    inputElement.classList.add(errorClass);
+    errorElement.textContent = validationMessage;
+}
+
+function hideInputError() {
+    const errorElement = form.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(inputErrorClass);
+    inputElement.classList.remove(errorClass);
+    errorElement.textContent = "";
+}
+
+function checkInputValidity(form, inputElement, options) {
+    if (!inputElement.validity.valid) {
+        showInputError(form, inputElement, inputElement.validationMessage, options);
+    } else {
+        hideInputError(form, inputElement, options);
+    }
+}
 
 function setEventListeners (form, options) {
     const {inputSelector} = options;
     inputList = [...form.querySelectorAll(inputSelector)];
-    inputList.addEventListener('input', (evt) => {
-     console.log(evt);
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', (evt) => {
+            checkInputValidity(form, inputElement, options);
+            toggleButtonState(inputList, buttonElement, options);
+           });
     });
+   
 }
 
 const enableValidation = (options) => {
@@ -18,14 +54,4 @@ const enableValidation = (options) => {
     });
 }
 
-const options = {
-    formSelector: ".modal",
-    inputSelector: ".modal__input",
-    submitButtonSelector: ".modal__save-button",
-    inactiveButtonClass: "modal__save-button_disabled",
-    inputErrorClass: "modal__input_type_error",
-    errorClass: "modal__error_visible"
-  };
-
 enableValidation(options);
-setEventListeneres(form, inputList, options);
